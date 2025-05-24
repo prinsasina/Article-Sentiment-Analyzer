1f2681978dca1c1bd1bea672333ce7c3688f73c4
prinsasina <prin.sasina@gmail.com>
2025-05-24 11:03:39 +0700

    Add text input

--- main.py
+++ main.py
@@ -3,33 +3,60 @@ from newspaper import Article
 import streamlit as st
 import matplotlib.pyplot as plt
 
-st.title("Article Sentiment Analyzer")
+st.title("SentimentScope")
+st.write("Text & Articles Sentiment Dashboard")
 
-url = st.text_input("Enter the URL of a news article: ")
+mode = st.radio("Select input method:", ["URL","Text"])
 
-if st.button("Analyze"):
-    if url:
-        try:
-            article = Article(url)
+if mode == "URL":
+    url = st.text_input("Enter the URL: ")
 
-            article.download()
-            article.parse()
-            article.nlp()
+    if st.button("Analyze"):
+        if url:
+            try:
+                article = Article(url)
 
-            st.subheader("Article Summary")
-            text = article.summary
-            st.write(text)
+                article.download()
+                article.parse()
+                article.nlp()
 
-            st.subheader("Sentiment Polarity")
-            blob = TextBlob(text)
-            sentiment = round(blob.sentiment.polarity * 100, 2)
-            st.write(f"{sentiment}%")
+                st.subheader("Article Summary")
+                text = article.summary
+                st.write(text)
 
-            graph, axis = plt.subplots()
-            axis.bar(["Sentiment"],[sentiment], color="red")
-            axis.set_ylim([-100,100])
-            axis.set_ylabel("Polarity (%)")
-            st.pyplot(graph)
-        
-        except Exception:
-            st.error("Failed to process the URL")
+                st.subheader("Sentiment Polarity")
+                blob = TextBlob(text)
+                sentiment = round(blob.sentiment.polarity * 100, 2)
+                st.write(f"{sentiment}%")
+
+                graph, axis = plt.subplots()
+                axis.bar(["Sentiment"],[sentiment], color="red")
+                axis.set_ylim([-100,100])
+                axis.set_ylabel("Polarity (%)")
+                st.pyplot(graph)
+            
+            except Exception:
+                st.error("Failed to process the URL")
+
+elif mode == "Text":
+    text_input = st.text_input("Enter the text: ")
+
+    if st.button("Analyze"):
+        if text_input:
+            try:
+                st.subheader("Text")
+                st.write(text_input)
+
+                st.subheader("Sentiment Polarity")
+                blob = TextBlob(text_input)
+                sentiment = round(blob.sentiment.polarity * 100, 2)
+                st.write(f"{sentiment}%")
+
+                graph, axis = plt.subplots()
+                axis.bar(["Sentiment"],[sentiment], color="red")
+                axis.set_ylim([-100,100])
+                axis.set_ylabel("Polarity (%)")
+                st.pyplot(graph)
+            
+            except Exception:
+                st.error("Failed to process the text")
